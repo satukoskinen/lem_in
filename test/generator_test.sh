d9cc14e2b903d56f7f0b562c5b4eaf39753b5787
Satu Koskinen <satu.a.koskinen@gmail.com>
2021-04-30 10:26:31 +0300

    test script

---
+++ test/generator_test.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+dir="gen_test_output"
+
+mkdir -p $dir
+
+repeat=10
+
+for (( i=0; i < $repeat; i++ ))
+do
+	echo $i
+	subdir="$dir/$i"
+	mkdir -p $subdir
+	map="$subdir/map_$i"
+	./test/generator --big-superposition > $map
+	grep "Here" -m 1 $map
+	output="$subdir/output_$i"
+	time ./lem_in < $map > $output
+	./lem_in_validator/lem_in_validator < $output
+done
