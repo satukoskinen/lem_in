2e4fc7ec837c387a48141b9057662417e44799e1
Satu Koskinen <skoskine@c1r5p9.hive.fi>
2021-05-11 15:47:48 +0300

    norme lem_in.h

--- lem_in.h
+++ lem_in.h
@@ -1,7 +1,7 @@
 #ifndef LEM_IN_H
 # define LEM_IN_H
 
-#include "core/inc/core.h"
+# include "core/inc/core.h"
 
 # define PRINT_DEBUG 0
 
@@ -42,10 +42,12 @@ typedef struct s_graph_attr
 
 t_graph			init_graph(void);
 t_edge_attr		*init_edge_attr(int capacity);
-t_node_attr		*init_node_attr(char *name, t_coordinates coordinates, t_graph_node *org);
+t_node_attr		*init_node_attr(char *name,
+					t_coordinates coordinates, t_graph_node *org);
 
 int				parse_input(t_graph *graph, t_parray *input);
-int				parse_line(t_graph *graph, t_parray *input, enum e_line_type *typ);
+int				parse_line(t_graph *graph, t_parray *input,
+					enum e_line_type *type);
 int				parse_link(t_graph *graph, char *line);
 int				parse_room(t_graph *graph, char *line, enum e_line_type *type);
 
@@ -57,9 +59,10 @@ ssize_t			lem_find_node(t_array *dst, t_graph_node *node);
 int				process_graph(t_parray *output, t_graph *graph);
 t_graph			lem_transform_vertex_disjoint(t_graph *src);
 t_array			find_max_flow_paths(t_graph *graph);
-int64_t			max_flow_edmonds_karp(t_array *path_combinations, t_graph_node *s,
-					t_graph_node *t);
-t_array			save_max_flow_paths(t_graph_node *s, t_graph_node *t, size_t max_flow);
+int64_t			max_flow_edmonds_karp(t_array *path_combinations,
+					t_graph_node *s, t_graph_node *t);
+t_array			save_max_flow_paths(t_graph_node *s, t_graph_node *t,
+					size_t max_flow);
 
 t_graph_edge	*lem_get_edge(t_graph_node *src, t_graph_node *dst);
 ssize_t			lem_compare_nodes(t_graph_node *n1, t_graph_node *n2);
